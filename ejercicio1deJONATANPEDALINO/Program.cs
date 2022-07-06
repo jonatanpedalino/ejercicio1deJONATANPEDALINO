f7f7b010478dfb65aae02259f94e2645aa89a2df
pamela <pamela@AIWA_NA-1413>
2022-07-05 23:12:36 -0300

    Agregar archivos de proyecto.

---
+++ ejercicio1deJONATANPEDALINO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ejercicio1deJONATANPEDALINO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float distanciarecorrida = 0f;
+            int metrosporsegundo = 0;
+            int segundostranscurridos = 0;
+            const float metroAkm = 0.001f;
+            Console.WriteLine("metros recorridos por segundo:");
+            metrosporsegundo = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("tiempo transcurrido en segundos:");
+            segundostranscurridos = Int32.Parse(Console.ReadLine());
+            distanciarecorrida = metrosporsegundo * segundostranscurridos;
+
+            Console.WriteLine("se recorrieron : " + distanciarecorrida + " metros en " + segundostranscurridos + " segundos.");
+            Console.WriteLine("se recorrieron:" + distanciarecorrida * metroAkm + "kms.");
+
+
+
+        }
+
+    }
+}
